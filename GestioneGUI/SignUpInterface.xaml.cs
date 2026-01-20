d3d314afce3d1dddfe268b05457e976c6925af1d
JoeleDePalma <joeledepalma135@gmail.com>
2026-01-20 21:43:11 +0100

    Migliora UI registrazione: toggle visibilità password
    Aggiunti stili personalizzati per PasswordBox e pulsante mostra/nascondi password. Introdotto StackPanel con PasswordBox, TextBox e pulsante con icona occhio per alternare la visibilità della password. Aggiunto testo di benvenuto e migliorato l’allineamento dei controlli. Aggiunto metodo SwapPasswordStackPanel per gestire la logica di visualizzazione password. Migliorata l’esperienza utente e la coerenza grafica.

--- GestioneGUI/SignUpInterface.xaml.cs
+++ GestioneGUI/SignUpInterface.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,7 +24,45 @@ namespace GestorePassword
         public SignUpInterface()
         {
             InitializeComponent();
+            Welcome_Text.Text = @"
+                        Benvenuto in Password Manager!
+                Crea il tuo account per iniziare a gestire 
+            le tue password in modo sicuro e semplice.
+        Password Manager ti offre un modo affidabile
+      per conservare tutte le tue credenziali in un unico posto, 
+    protette da una crittografia avanzata dei dati,
+   inoltre offre un'interfaccia semplice e chiara, 
+  per permettere a tutti di comprendere 
+  e sfruttare tutte le funzionalità presenti!
+  Inizia subito creando il tuo account!
+  Sei già registrato?
+";
         }
+
+        public void SwapPasswordStackPanel(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox_Input.Visibility == Visibility.Visible)
+            {
+                TextBox_Input.Text = PasswordBox_Input.Password;
+
+                PasswordBox_Input.Visibility = Visibility.Collapsed;
+                TextBox_Input.Visibility = Visibility.Visible;
+
+                Image_PasswordState.Source = new BitmapImage(new Uri("/Images/opened_eye.png", UriKind.Relative));
+            }
+
+            else
+            {
+                PasswordBox_Input.Password = TextBox_Input.Text;
+
+                PasswordBox_Input.Visibility = Visibility.Visible;
+                TextBox_Input.Visibility = Visibility.Collapsed;
+
+                Image_PasswordState.Source = new BitmapImage(new Uri("/Images/closed_eye.png", UriKind.Relative));
+
+            }
+        }
+
     }
 }
 
