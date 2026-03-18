a96d44b967ff47ce368ea19b708dd8b0d4263043
JoeleDePalma <joeledepalma135@gmail.com>
2026-03-18 22:20:39 +0100

    Adattato il progetto GUI alle prossime modifiche: rifattorizzata struttura del progetto e aggiunta classe UserInfo

---
+++ GestioneGUI/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestioneGUI
+{
+    public class UserInfo
+    {
+        public int CredentialID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
