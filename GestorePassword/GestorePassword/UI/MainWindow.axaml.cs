48dadba3b89802c7ecd7b87a56112dbab12f518f
JoeleDePalma <joeledepalma135@gmail.com>
2026-04-18 18:26:24 +0200

    Aggiornate e aggiunte immagini. Finita la creazione del grid delle password, iniziato grid delle statistiche. Creato template per le password e cartella dedicata agli stili dei template.

--- GestorePassword/GestorePassword/UI/MainWindow.axaml.cs
+++ GestorePassword/GestorePassword/UI/MainWindow.axaml.cs
@@ -1,14 +1,19 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace GestorePassword;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+
+        MinWidth = 800;
+        MinHeight = 450;
     }
 
     public void ChangeInterface(UserControl newInterface)
